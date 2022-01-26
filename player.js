c6afa0a8f5b9546ff8993a004b179a477236a27e
Shira <shafirshira@gmail.com>
2022-01-26 11:47:24 +0200

    first draft, there are 4 tips, creating and designing tips and looking where to place each

--- player.js
+++ player.js
@@ -13,7 +13,7 @@ const loadJquery = async () =>{
 
 await loadJquery();
 
-loadCSS = function (){
+function loadCSS(){
     $("document.createElement(\"link\")", {
         rel: "stylesheet",
         type: "text/css",
@@ -21,3 +21,59 @@ loadCSS = function (){
     }).appendTo("head");
 }
 loadCSS();
+
+
+//creating a tip content based on content and id
+const setTipContent =(content, id) => {
+    return `<div class="sttip" id="x_${id}" style="position: absolute;" xmlns="http://www.w3.org/1999/html">
+    <div  class="sttip">
+     <div class="tooltip in">
+           <div class="tooltip-arrow">  <button onclick="prevTool(${id})" style="font-size: medium;background-color: azure"> < </button> </div>
+           <div class="tooltip-arrow second-arrow">           <button onclick="nextTool(${id})" style="font-size: medium;background-color: azure"> > </button> </div>
+           <div class="popover-inner" id="closing_${id}" style="animation: ease-out; background-color: darkcyan; font-size: large">
+           ${content}
+           <button onclick="closerTool(${id})" style="font-size: medium;background-color: azure">X</button>
+                </div>
+             </div>
+           </div>`
+};
+
+
+function prevTool(id){
+    if (id !== 0) {
+        $("#tooltip-arrow_" + id).prev(id);
+        $("#tooltip-arrow_" + id).hide();
+    }
+}
+
+function nextTool(id){
+    if (id !== 5) {
+        $("#tooltip-arrow_" + id).next(id);
+        $("#tooltip-arrow_" + id).hide();
+    }
+}
+
+//closes tooltips using button on each of them
+const closerTool = (id) =>{
+    $("#closeing_" + id).hide();
+};
+
+//placing content based on location
+const placeTipLocation = (content) => {
+    if (content.contains("Welcome")){
+        $(content).css({"margin-top":"auto","display":"flex","flex-direction":"column","align-items":"center"})
+    }
+    else if(content.contains("Image")){
+        $(content).css({"float":"right","display": "flex"})
+    }
+    else if (content.contains("Enter")){
+        $(content).css({"margin":"0 auto","display": "block","height":"70px","padding-top":"18px"})
+    }
+    else if(content.contains("to search")){
+        $(content).css({"margin":"0 auto","display": "block"})
+    }
+}
+
+const tooltipData  = () =>{
+
+}

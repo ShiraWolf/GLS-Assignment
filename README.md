b47e52f7fe999570966efbfe5226ae1e9bd48957
Shira <shafirshira@gmail.com>
2022-01-30 16:54:53 +0200

    fixed location bug, changed prev button color and at step 4 next text to finish

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Please follow these instructions to run it:
 3. Right click on the mouse and "inspect"
 4. Click console
 5. Paste player.js and press Enter
+6. Please make sure your screen is at 100% and close the Console afterward for full view
--- player.js
+++ player.js
@@ -19,8 +19,7 @@ let loadStyle = function(url) {
     styleTag.href = url;
     document.head.appendChild(styleTag);
 };
-//style='position:fixed; top: 50px; left: 50px'
-// style='display: flex; position: absolute; align-items: center'
+
 //creating a tip based on tip css
 const setTipTool =(toolCSS,tooltip) => {
     $("head").append('<style>'+toolCSS+'</style>')
@@ -39,22 +38,22 @@ const setTipTool =(toolCSS,tooltip) => {
     $("button[data-iridize-role='laterBt']").click(closeBtn)
     $("button[data-iridize-role='closeBt']").css({"padding": '0px 25px'}).click(closeBtn);
     $("a[data-iridize-role='nextBt']").click(nextBt);
-    $("button[data-iridize-role='prevBt']").click(prevBt).css({"padding": '0px 15px'});
+    $("button[data-iridize-role='prevBt']").click(prevBt).css({"padding": '0px 14px',"background-color":"#7dacb3"});
     toolTipContent();
 };
 
 function closeBtn(){
-    $('.sttip').hide();
+    $('.sttip').remove();
 }
 
 function nextBt(){
     ++currStep;
     toolTipContent();
     if (currStep > 1){
-        $("button[data-iridize-role='prevBt']").removeClass('default-prev-btn');
+        $("button[data-iridize-role='prevBt']").removeClass('default-prev-btn');//prevents from going into step 0 or minus and clicking button
     }
     if (currStep === steps.length-1){
-        $("a[data-iridize-role='nextBt']").hide();
+        $("a[data-iridize-role='nextBt']").text("Finish");// change text and remove content when pressed
     }
 }
 
@@ -79,39 +78,27 @@ function toolTipContent(){
         locationTip();
     }
     else{
-        $('.sttip').remove();
+        $('.sttip').hide();
     }
     $("div[data-iridize-id='content']").html(content);
 }
 
+//base tip with content on the right location
 function locationTip(){
     const action = steps[currStep-1].action
     let content = action.contents["#content"];
-    $("sttip">"popover-inner").css({"position": "absolute","top":"50px","right":"20px"})
     if (content.includes("Welcome")){
-        console.log("welcome")
-        $(`#x_${steps[currStep-1].id}`).css({
-            position: "absolute",
-            display: "block",
-            bottom: '',
-            right: 300,
-            top: 70
-        })
+        $('.sttip').css({position: "absolute",display: "block",left: "50%",top: "10%"})
     }
     else if(content.includes("Image")){
-        console.log("img")
-        $(content).css({"position": "absolute","top":"50px","right":"20px"})
+        $('.sttip').css({"position": "absolute","top":"40px","left":"80%"})
     }
     else if (content.includes("Enter")){
-        console.log("ent")
-        $(content).css({"margin":"0 auto","display": "block","height":"70px","padding-top":"18px"})
+        $('.sttip').css({"position": "absolute","top":"35%","left":"15%"})
     }
     else if(content.includes("to search")){
-        console.log("sr")
-        $(content).css({"margin":"0 auto","display": "block"})
+        $('.sttip').css({'position': "absolute","top":"55%","left":"35%"})
     }
-
-
 }
 
 //injected setTipContent to html
